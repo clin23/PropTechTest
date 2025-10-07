9d0423756f2bd956e15d268b7751b4de4328e522
clin23 <139291454+clin23@users.noreply.github.com>
2025-10-08 08:19:04 +1100

    Expand other income search input width

--- components/IncomesTable.tsx
+++ components/IncomesTable.tsx
@@ -352,11 +352,11 @@ export default function IncomesTable({
           </span>
           <input
             type="search"
-            className={`${filterControlClass} w-full min-w-[12rem] pl-10`}
-            placeholder="Search income"
+            className={`${filterControlClass} w-full min-w-[24ch] pl-10`}
+            placeholder="Search for other income"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            aria-label="Search income"
+            aria-label="Search for other income"
           />
         </div>
       </div>
